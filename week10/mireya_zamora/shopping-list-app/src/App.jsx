644b32e35b55582b72caff5c96e6e263c20f0a7f
MireyaZamora <mireyazamora@berkeley.edu>
2025-04-17 20:55:02 -0700

    week 10
    fixed week 9 + added a bit to week 10

--- week10/mireya_zamora/shopping-list-app/src/App.jsx
+++ week10/mireya_zamora/shopping-list-app/src/App.jsx
@@ -1,39 +1,3 @@
-import { useState } from week9\shopping-list-app\node_modules\react
-import reactLogo from week9\shopping-list-app\src\assets\react.svg
-import viteLogo from week9\shopping-list-app\public\vite.svg
-import week9\shopping-list-app\public\vite.svg
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-////export default App
-
 import { useState } from 'react';
 import './App.css'
 import ShoppingList from './ShoppingList';
@@ -69,7 +33,7 @@ function App() {
            <div className='card'>
                <form onSubmit={addItem} className='flex-apart'>
                    <input type="text" name="name" placeholder='Add item to list...' />
-                   {/* { create another input that allows people to add costs for an item */} */}
+                   {/* create another input that allows people to add costs for an item */}
                    <button className='btn purple' type='submit'>Add</button>
                </form>
            </div>
@@ -83,6 +47,5 @@ function App() {
        </>
    );
 }
-
 
 export default App;
